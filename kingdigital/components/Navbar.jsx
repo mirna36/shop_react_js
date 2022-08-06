13556c48aa385bbd55a1603427a6ac69fe6f986a
mirna36 <69789618+mirna36@users.noreply.github.com>
2022-08-06 15:57:46 +0200

    création de la page produits

--- kingdigital/components/Navbar.jsx
+++ kingdigital/components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">Kingdigital Casque/Ecouteurs</Link>      
+        </p>
+        <button type ="button" className="cart-icon" onClick=""></button>
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+
+    </div>
   )
 }
 
--- kingdigital/pages/index.js
+++ kingdigital/pages/index.js
@@ -1,23 +1,35 @@
 import React from 'react';
+
+import { client} from '../lib/client';
 import { Product, FooterBanner, HeroBanner} from '../components';
 
-const Home = () => {
-  return (
-    <>
-      <HeroBanner />
-      <div className="products-heading">
-        <h2>Beset Selling Products</h2>
-        <p>Speakers of many variations</p>
-      </div>
-      <div className="products-container">
-        {['Product 1', 'Product 2'].map((product) => product)}
-      </div>
+const Home = ({ products, bannerData }) => (
+  <div>
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
 
-      <FooterBanner />
+    <div className="products-heading">
+      <h2>Meilleur Vendeur de Produits Hi-Tech!</h2>
+      <p>Un grand catalogue de modèles des derniers produits digitaux</p>
+    </div>
+    <div className="products-container">
+      {products?.map((product) => <Product Key={product.id} product={product}/>)}
+    </div>
 
+    <FooterBanner footerBanner={bannerData && bannerData[0]}/>
+  </div>
+);
 
-    </>
-  )
+  export const getServerSideProps = async() =>{
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props:{ products, bannerData }
+  }
 }
+
 
 export default Home;
