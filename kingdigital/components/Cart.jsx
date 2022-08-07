a3be3cf3047a617f7ca6734799beefd85aa5f438
mirna36 <69789618+mirna36@users.noreply.github.com>
2022-08-07 15:31:39 +0200

    panier

--- kingdigital/components/Cart.jsx
+++ kingdigital/components/Cart.jsx
@@ -1,9 +1,112 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
+
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
+
+
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext()
+  
+    const tva = totalPrice * 0.119;
+    const soustotal = totalPrice - tva;
+  
   return (
-    <div>Cart</div>
-  )
+    <div className="cart-wrapper" ref={cartRef}>
+      <div className="cart-container">
+        <button
+          type="button"
+          className="cart-heading"
+          onClick={() => setShowCart(false)}
+        >
+          <AiOutlineLeft />
+          <span className="heading">Votre panier</span>
+          <span className="cart-num-items">({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className="empty-cart">
+            <AiOutlineShopping size={150} />
+            <h3>Votre panier est vide.</h3>
+            <Link href="/">
+              <button
+                type="button"
+                onClick={() => setShowCart(false)}
+                className="btn"
+              >
+                Continuer vos achats
+              </button>
+            </Link>
+          </div>
+        )}
+        <div className="product-container">
+          {cartItems.length >= 1 &&
+            cartItems.map((item) => (
+              <div className="product" key={item._id}>
+                <img
+                  src={urlFor(item?.image[0])}
+                  className="cart-product-image"
+                />
+                <div className="item-desc">
+                  <div className="flex top">
+                    <h5>{item.name}</h5>
+                    <h4>{item.price}€</h4>
+                  </div>
+                  <div className="flex bottom">
+                    <div>
+                      <p className="quantity-desc">
+                        <span className="minus" onClick="">
+                          <AiOutlineMinus />
+                        </span>
+                        <span className="num" onClick>
+                          0
+                        </span>
+                        <span className="plus" onClick="">
+                          <AiOutlinePlus />
+                        </span>
+                      </p>
+                    </div>
+                    <button type="button" className="remove-item" onClick="">
+                      <TiDeleteOutline />
+                    </button>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+        {cartItems.length >= 1 && (
+          <div className="cart-bottom">
+            <div className="total">
+              <h3>Sous-Total:</h3>
+              <h3>{soustotal.toFixed(2)} €</h3>
+            </div>
+            <div className="total">
+              <h3>TVA:</h3>
+              <h3>{tva.toFixed(2)}€</h3>
+            </div>
+            <div className="total">
+              <h3>Total:</h3>
+              <h3>{totalPrice}€</h3>
+            </div>
+            <div className="btn-container">
+            <button
+            type="button"
+            className="btn"
+            onClick="">
+            Paiement
+
+            </button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Cart
