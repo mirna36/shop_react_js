d94b370ab6fda359b8e65d169af617dfa4238ace
mirna36 <69789618+mirna36@users.noreply.github.com>
2022-08-08 23:53:39 +0200

    installation paiement stripe

--- kingdigital/components/Cart.jsx
+++ kingdigital/components/Cart.jsx
@@ -6,6 +6,7 @@ import toast from 'react-hot-toast';
 
 import { useStateContext } from '../context/StateContext';
 import { urlFor } from '../lib/client';
+import getStripe from '../lib/getStripe';
 
 
 
@@ -19,6 +20,25 @@ const Cart = () => {
     toggleCartItemQuantity,
     onRemove
   } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe();
+
+    const response = await fetch('/api/stripe',{
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+
+    if(response.statusCode === 500) return;
+
+    const data = await response.json();
+    toast.loading('Redirecting...');
+    stripe.redirectToCheckout({ sessionId: data.id });
+
+  }
   
     const tva = totalPrice * 0.119;
     const soustotal = totalPrice - tva;
@@ -111,7 +131,8 @@ const Cart = () => {
               <h3>{totalPrice.toFixed(2)}€</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
+              <button type="button" className="btn" onClick={
+                handleCheckout}>
                 Paiement
               </button>
             </div>
